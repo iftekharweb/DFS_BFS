799b398180c1498e98670e63ca1e3649169539d6
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-05-10 23:36:29 +0600

    Create Components.cpp

---
+++ Components.cpp
@@ -0,0 +1,10 @@
+
+//          U   R  D  L
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+
+bool is_valid(ll x , ll y) {
+    if(x>n-1 || x<0 || y>m-1 || y<0) return false;
+    return true;
+}
