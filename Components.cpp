f5182699ef1e82febef7725b1cd3d80001cbab01
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-07-24 17:38:32 +0600

    Update Components.cpp

--- Components.cpp
+++ Components.cpp
@@ -8,3 +8,10 @@ bool is_valid(ll x , ll y) {
     if(x>n-1 || x<0 || y>m-1 || y<0) return false;
     return true;
 }
+
+
+const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+
+const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
